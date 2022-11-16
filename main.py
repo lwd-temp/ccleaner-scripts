67ce4249ebd87f31855e4f0a217b4806799bdb3c
Sunset <26019675+lwd-temp@users.noreply.github.com>
2022-11-16 04:50:16 +0000

    bug fix

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def make_archive(source, destination):
     base = os.path.basename(destination)
     name = base.split('.')[0]
     format = base.split('.')[1]
-    archive_from = os.path.dirname(source)
+    archive_from = os.path.dirname(os.path.abspath(source))
     archive_to = os.path.basename(source.strip(os.sep))
     print(source, destination, archive_from, archive_to)
     shutil.make_archive(name, format, archive_from, archive_to)
